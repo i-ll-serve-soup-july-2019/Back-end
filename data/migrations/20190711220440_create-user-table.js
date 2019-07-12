ba3176eb46de67c2002c77d27a3c96356efd0dd0
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-07-11 22:56:38 -0500

    added dev and test db/created user table migration for both db's

--- data/migrations/20190711220440_create-user-table.js
+++ data/migrations/20190711220440_create-user-table.js
@@ -2,7 +2,10 @@
 exports.up = function(knex) {
   return knex.schema.createTable('users', tbl => {
       tbl.increments()
-
+      tbl.string('name', 255).notNullable()
+      tbl.string('email', 32).notNullable().unique()
+      tbl.string('password', 32).notNullable()
+      tbl.string('role', 32)
   })
 };
 
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 
-//********* update and uncomment once server file made
-//const server = require('add path here    ./api/server.js')
+
+const server = require('./api/server.js')
 
 const port = process.env.PORT || 7777
-//****/uncomment when server made
-//server.listen(port, () => console.log(`\n*** Let's get some soup on port ${port} ***\n`))
+//****/uncomment when server madenpr
+server.listen(port, () => console.log(`\n*** Let's get some soup on port ${port} ***\n`))
