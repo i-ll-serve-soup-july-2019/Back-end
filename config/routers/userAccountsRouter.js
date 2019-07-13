48e4d6b4adce12e4cdaaa4a764aee16d0d7d3276
jthicks91 <jordanthicks91@gmail.com>
2019-07-12 19:40:51 -0700

    userAccounts router for login and register endpoints.

---
+++ config/routers/userAccountsRouter.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const knex = require("knex");
+const server = express();
+const bcrypt = require("bcryptjs");
+const knexConfig = require("../../knexfile.js");
+const db = knex(knexConfig.development);
+const middleware = require("../middleware.js");
+const generateToken = middleware.generateToken;
+
+server.post("/register", (req, res) => {
+  const newUser = req.body;
+
+  userInfo.password = bcrypt.hashSync(userInfo.password, 14);
+  if (userInfo.id) {
+    res.status(400).json({
+      error:
+        "Please do not include the ID number in the registration. The system auto-generates them"
+    });
+  } else {
+    db("users")
+      .insert(newUser)
+      .then(ids => {
+        res.status(201).json({
+          message: `User ${userInfo.username} has been successfully registered`,
+          userID: ids[0]
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          err,
+          message: "There has been an error on the Register POST endpoint"
+        });
+      });
+  }
+});
+
+server.post("/login", (req, res) => {
+  const userInfo = req.body;
+
+  db("users")
+    .where({ username: userInfo.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(userInfo.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: user.username,
+          token: token
+        });
+      } else {
+        res.status(401).json({
+          message: "Incorrect username and/or password."
+        });
+      }
+    })
+    .catch(err =>
+      res.status(500).json({
+        err,
+        message: "There has been an error on the Login POST endpoint"
+      })
+    );
+});
