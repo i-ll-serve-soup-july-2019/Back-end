f367a1510859b33089ddbd7cbfca888c17832d41
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-07-12 19:37:41 -0500

    added username row to test and dev db

--- data/migrations/20190711220440_create-user-table.js
+++ data/migrations/20190711220440_create-user-table.js
@@ -4,6 +4,7 @@ exports.up = function(knex) {
       tbl.increments()
       tbl.string('name', 255).notNullable()
       tbl.string('email', 32).notNullable().unique()
+      tbl.string('username', 32).notNullable().unique()
       tbl.string('password', 32).notNullable()
       tbl.string('role', 32)
   })
