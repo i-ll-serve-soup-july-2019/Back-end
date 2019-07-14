35bb288bec70bc45abd8f498f8cff16c159346b6
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-07-14 14:00:53 -0500

    added addUser and findUser(filter) helper functions

--- users/users-model.js
+++ users/users-model.js
@@ -3,4 +3,19 @@ const db = require('../data/dbConfig.js')
 module.exports = {
     addUser,
     findUserBy,
+    findUserById
+    }
+
+
+async function findUserBy(filter){
+    return db('users').where(filter)
+}
+
+async function findUserById(id) {
+    return db('users').where({ id }).first()
+}
+
+async function addUser(user){
+    const [id] = await db('users').insert(user)
+    return findUserById(id)
 }
--- users/users-model.spec.js
+++ users/users-model.spec.js
@@ -3,4 +3,30 @@ const Users = require('./users-model.js')
 
 describe('users model functions', () => {
     
+    describe('addUser function', () => {
+        afterEach( async () => {
+            await db ('users').truncate()
+        })
+
+        it('should add new user to the db', async () => {
+            const newUser1 = {
+                name:"new1",
+                email:"new1@user.com",
+                username:"newUser1",
+                password:"password",
+                role:"test"
+            }
+
+            await Users.addUser(newUser1)
+            const userList = await db('users')
+
+            expect(userList).toHaveLength(1)
+            expect(userList[0].name).toBe(newUser1.name)
+            expect(userList[0].email).toBe(newUser1.email)
+            expect(userList[0].username).toBe(newUser1.username)
+            expect(userList[0].role).toBe(newUser1.role)
+            //bcrypt hash password in endpoint
+        })
+    })
+
 })
