baed770b7de3115aac31939a6896e0a91ade2451
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-07-16 00:06:51 -0500

    created inventory table migration with foreign keys ref to users table

---
+++ data/migrations/20190715204308_create-inventory-table.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('inventory', tbl => {
+    tbl.increments('id').primary()
+    //foreign keys
+    tbl.integer('userId').references('id').inTable('users').notNull().onDelete('cascade')
+    tbl.string('username').references('username').inTable('users').notNull()
+    //refer to user id and username from users table
+    tbl.string('item', 32).notNullable()
+    tbl.integer('quantity').defaultTo(0)
+    tbl.string('units', 32).notNullable()
+    tbl.integer('threshold')
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('inventory')
+};
