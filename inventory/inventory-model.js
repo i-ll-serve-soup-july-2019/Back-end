793b48bd24746fbc12ce26107eda66d89ef08ba5
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-07-16 21:48:32 -0500

    initial commit for inventory model/added inventory tbl to test db

---
+++ inventory/inventory-model.js
@@ -0,0 +1,29 @@
+const db = require('../data/dbConfig.js')
+
+module.exports = {
+    getInventory,
+    addItem,
+    updateItem,
+    findBy,
+    findById
+}
+
+async function getInventory(){
+    return null
+}
+
+async function findBy(filter){
+    return null
+}
+
+async function findById(id){
+    return null
+}
+
+async function addItem(item){
+    return null
+}
+
+async function updateItem(item){
+    return null
+}
---
+++ inventory/inventory-model.spec.js
@@ -0,0 +1,19 @@
+const db = require('../data/dbConfig.js')
+const Users = require('./users-model.js')
+
+describe('inventory model functions', () => {
+
+    describe('get inventory', () => {
+
+    })
+
+    describe('add item to inventory', () => {
+
+    })
+
+    describe('update inventory item', () => {
+
+    })
+
+
+})
